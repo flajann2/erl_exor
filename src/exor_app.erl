e201e515dd6e07b4230d4625fb275ca0f301b3cb
Fred Mitchell <fred.mitchell@gmx.de>
2015-09-13 21:13:13 +0200

    gen_event in prep for handling Rabbit events

--- src/exor_app.erl
+++ src/exor_app.erl
@@ -6,13 +6,18 @@
 -module('exor_app').
 -include_lib("amqp_client/include/amqp_client.hrl").
 
--behaviour(application).
-
-%% Application callbacks
-%%-export([start/2 ,stop/1]).
+%%-behaviour(application).
+-behaviour(gen_event).
 
 %% Event callbacks
--export([init/1, handle_event/2, terminate/2]).
+-export([start/2, 
+         stop/1,
+         init/1, 
+         handle_event/2,
+         terminate/2,
+         code_change/3,
+         handle_call/2,
+         handle_info/2]).
 
 %%====================================================================
 %% appication API
@@ -31,7 +36,6 @@ stop(_State) ->
 %% gen_event API
 %%====================================================================
 
-
 init(_Args) ->
     io:fwrite("event init called!!!\n\n"),
     {ok, []}.
@@ -43,6 +47,19 @@ handle_event(ErrorMsg, State) ->
 terminate(_Args, _State) ->
     io:fwrite("event terminate called!!!\n\n"),
     ok.
+
+code_change(_OldVersion, State, _Extra) ->
+    io:fwrite("code_change called!!!\n\n"),
+    {ok, State}.
+
+handle_call(_Msg, State) ->
+    io:fwrite("handle_call called!!!\n\n"),
+    Reply = ok,
+    {ok, Reply, State}.
+
+handle_info(_Msg, State) ->
+    io:fwrite("handle_info called!!!\n\n"),
+    {ok, State}.
 
 %%====================================================================
 %% Internal functions
